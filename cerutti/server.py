a87db64dbdbcbefa6ffe13cda3da9f63ebce7ee2
Alexander Jackson <alexanderjackson@protonmail.com>
2021-03-26 11:49:34 +0000

    feat: allow multiple rooms
    When users connect to the websocket, move them to the room they request
    and only begin games when each room has enough bots. Once all websockets
    have been informed of the winners, delete the room so it can be used
    again.
    
    This allows clients to specify a gametype and room size and be matched
    up only when enough people choose the same settings.

--- cerutti/server.py
+++ cerutti/server.py
@@ -3,54 +3,98 @@
 import asyncio
 import websockets
 
+from dataclasses import dataclass
+from typing import Dict, Optional, List, Tuple
+
 from zenlog import log
 
 from cerutti.lib.auctioneer import Auctioneer
 from cerutti.lib.user_bot import UserBot
 from cerutti.lib.messages import AuctionEnd, Registration
 
-auctioneer = None
-room = []
-winners = []
+Room = List[UserBot]
+
 event = asyncio.Event()
-game_lock = asyncio.Lock()
-game_has_been_run = False
+
+
+@dataclass
+class RoomKey:
+    gametype: str
+    bots: int
+
+
+@dataclass
+class RoomInfo:
+    has_run: bool
+    winners: Optional[List[str]]
+    event: asyncio.Event
+    finished: int
+
+
+rooms: Dict[RoomKey, Tuple[asyncio.Lock, Room, RoomInfo]] = {}
 
 
 async def root(websocket, path):
-    global game_has_been_run, winners
-
     message = await websocket.recv()
     registration = Registration.Schema().loads(message)
     log.debug(f"Received a connection with settings={registration}")
 
+    key = RoomKey(gametype=registration.gametype, bots=registration.bots)
+
+    # Ensure the room exists
+    if key not in rooms:
+        log.debug(f"Adding room_key={key} to the state")
+
+        rooms[key] = (
+            asyncio.Lock(),
+            [],
+            RoomInfo(has_run=False, winners=None, event=asyncio.Event(), finished=0),
+        )
+
+    # Get the room itself and add the current client
+    lock, room, info = rooms[key]
+    room.append(UserBot(registration.name, websocket))
+
     greeting = (
         f"Hello {registration.name}, there are currently {len(room)} bots playing!"
     )
+
     await websocket.send(greeting)
 
-    room.append(UserBot(registration.name, websocket))
-
     # If we don't have enough bots, wait until we do
-    if len(room) < registration.bots:
-        await event.wait()
+    if len(room) < key.bots:
+        log.debug(f"Room with key={key} has too few bots currently")
+        await info.event.wait()
 
     # We have enough, notify all tasks
-    event.set()
+    info.event.set()
 
-    async with game_lock:
-        if not game_has_been_run:
+    async with lock:
+        if not info.has_run:
+            log.debug(f"Beginning auction for key={key}")
+
             # Create the auctioneer and begin
             auctioneer = Auctioneer(
-                room=room, game_type=registration.gametype, slowdown=0, verbose=True
+                room=room,
+                game_type=key.gametype,
+                slowdown=0,
+                verbose=True,
             )
-            winners = await auctioneer.run_auction()
-            log.info(f"Winners: {winners}")
 
-            game_has_been_run = True
+            info.winners = await auctioneer.run_auction()
+            log.info(f"Winners: {info.winners}")
 
-    registration = AuctionEnd(winners=winners)
-    message = AuctionEnd.Schema().dumps(registration)
+            info.has_run = True
+
+        info.finished += 1
+
+    auction_end = AuctionEnd(winners=info.winners)
+    message = AuctionEnd.Schema().dumps(auction_end)
+
+    # If all sockets have informed their clients, delete the room
+    if info.finished == key.bots:
+        log.debug(f"All {info.finished} clients have been informed, deleting key={key}")
+        del rooms[key]
 
     # Game has been run now, inform the sockets
     await websocket.send(message)
